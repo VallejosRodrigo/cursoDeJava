bd948e7f597d0b422a371ee661231ac8845b318b
Rodrigo V <vallejos.rodrigo@outlook.com>
2022-08-23 15:48:27 -0300

    Trabajo practico 4

---
+++ tp4LAB/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nombre = "Vallejos Rodrigo";
+        int edad = 28;
+        double salario = 286000.4;
+        boolean carnet = true;
+
+
+    }
+}
